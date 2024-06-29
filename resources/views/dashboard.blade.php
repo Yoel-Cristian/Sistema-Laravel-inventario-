456a1e08e2008fac362729e19525ecb81cb4f342
Yoel-Cristian-Quispe-Diaz <e117303@uajms.edu.bo>
2024-06-28 23:56:09 -0400

    CRUD DE PROFILES_PRIMERA PARTE

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('PAGINA PRINCIPAL DE YOEL QUISPE') }}
+            {{ __('inicio') }}
         </h2>
     </x-slot>
 
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -29,6 +29,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('profile.principal') }}" :active="request()->routeIs('profile.principal')">
+                        {{ __('PERFILES') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
